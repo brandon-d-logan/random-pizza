4770447dfd4d7023547cc1ec1228faaa0331cad7
Brandon <loganb1@ohio.edu>
2023-12-28 15:29:18 -0500

    Rewrite in vanilla JS
    Remove the jQuery dependence, update selectors based on the new Avalanche site at https://order.toasttab.com/online/avalanche-pizza#!/

--- avalanche-random-pizza.js
+++ avalanche-random-pizza.js
@@ -1,13 +1,26 @@
-  var randomBtn = jQuery('<a href="#" id="random-pizza" style="position:fixed;top:100px;right:100px;padding:30px;text-align:center;background:red;color:#fff;border-radius:10px;font-size:16px;">Random Pizza!</a>');
-  jQuery('body').prepend(randomBtn);
-  jQuery('body').on('click', '#random-pizza', function(){
-    /*Clear any previous selected*/
-    jQuery('span.this-one').removeClass('this-one').attr('style','');
-    var counter = 0;
-     /*Highlight a random pizza*/
-    jQuery('span.title.red').each(function(){counter++;jQuery(this).attr('id',counter);});
-    function getPizza(min, max) {return Math.random() * (max - min) + min;}
-    var pizza = Math.ceil(getPizza(1,counter));
-    jQuery('span#'+pizza).attr('style','display:block;padding:5px;background:#d91000;color:#fff;').addClass('this-one');
-    jQuery('body,html').animate({scrollTop:jQuery('span.this-one').offset().top-120}, 'slow');
-   });
+var styles = document.createElement("style");
+styles.innerHTML = ".this-one{border:5px solid red !important;margin-right:-10px;}";
+document.body.appendChild(styles);
+function randomNumber(min, max){ return Math.random() * (max - min) + min; }
+function scrollTo(className, spaceAbove) {
+    // Default spaceAbove to 200 pixels if not provided
+    spaceAbove = spaceAbove || 200;
+    var el = document.querySelector("." + className);
+    if (el) { var scrollPosition = el.getBoundingClientRect().top - spaceAbove; window.scrollBy({ top: scrollPosition, left: 0, behavior: "smooth" }); }
+}
+function randomBtnClick(){
+  //clear any selected items
+  var selected = document.getElementsByClassName('this-one');if(selected){ for (var i = 0; i < selected.length; i++){ selected[i].classList.remove('this-one'); } }
+  var pizzas = document.querySelectorAll('.pageContent .menu .menuGroup:nth-child(5) .itemSection li');
+  //get random one out of the set
+  var pizzaIndex = Math.ceil(randomNumber(1,pizzas.length));
+  //add styling class and scroll to the chosen item
+  pizzas[pizzaIndex].classList.add('this-one');
+  scrollTo("this-one");
+}
+var randomBtn = document.createElement("button");
+randomBtn.id = "random-pizza";
+randomBtn.style = "z-index:99999 !important;position:fixed;top:100px;right:100px;padding:30px;text-align:center;background:red;color:#fff;border-radius:10px;font-size:16px;";
+randomBtn.innerText = "Random Pizza!";
+randomBtn.addEventListener("click", randomBtnClick);
+document.body.prepend(randomBtn);
